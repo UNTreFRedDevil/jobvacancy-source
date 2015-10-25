7d61f3d13c6dcbd2ce47984ff76e7afdb7916aee
Diego Tabares <dtabares@gmail.com>
2015-10-25 20:38:35 -0300

    agrego mas escenarios de prueba como mails con espacios a emailUtil test

--- src/test/java/com/jobvacancy/web/rest/EmailUtilTest.java
+++ src/test/java/com/jobvacancy/web/rest/EmailUtilTest.java
@@ -14,18 +14,18 @@ public class EmailUtilTest {
     @BeforeClass
     public static void emailProviderText() {
         validEmails = new String[] { "test@example.com",
-            "test-101@example.com", "test.101@yahoo.com",
+            "test-101@example.com","test+101@example.com" , "test.101@yahoo.com",
             "test101@example.com", "test_101@example.com",
-            "test-101@test.net", "test.100@example.com.au", "test@e.com",
+            "test-101@test.net", "test.100@example.com.au", "test@e.com.ar",
             "test@1.com", "test@example.com.com", "test+101@example.com",
             "101@example.com", "test-101@example-test.com" };
 
         invalidEmails = new String[] { "example", "example@.com.com",
             "exampel101@test.a", "exampel101@.com", ".example@test.com",
-            "example**()@test.com", "example@%*.com",
+            "example**()@test.com", "example@%*.com","@example","example@",
             "example..101@test.com", "example.@test.com",
             "test@example_101.com", "example@test@test.com",
-            "example@test.com.a5" };
+            "example@test.com.a5","example@example","    @test.com" };
     }
 
     @Test
