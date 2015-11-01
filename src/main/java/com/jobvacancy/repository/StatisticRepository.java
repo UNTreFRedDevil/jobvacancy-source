e33de797861fbb7ec45424c4fb1e003917868ba5
Diego Tabares <dtabares@gmail.com>
2015-11-01 20:03:46 -0300

    remuevo import no utilizado

--- src/main/java/com/jobvacancy/repository/StatisticRepository.java
+++ src/main/java/com/jobvacancy/repository/StatisticRepository.java
@@ -3,8 +3,6 @@ package com.jobvacancy.repository;
 import com.jobvacancy.domain.Statistic;
 import org.springframework.data.jpa.repository.*;
 
-import java.util.List;
-
 /**
  * Spring Data JPA repository for the Statistic entity.
  */
