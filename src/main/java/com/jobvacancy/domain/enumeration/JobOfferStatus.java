72b3a44d963c66b7742b54a5ac8f7933011fd747
Santiago Rojo <tiagox@gmail.com>
2015-11-17 18:22:53 -0300

    Add status field for job offers

---
+++ src/main/java/com/jobvacancy/domain/enumeration/JobOfferStatus.java
@@ -0,0 +1,8 @@
+package com.jobvacancy.domain.enumeration;
+
+/**
+ * The JobOfferStatus enumeration.
+ */
+public enum JobOfferStatus {
+    AVAILABLE, JOBVACANCY_HIRED, EXTERNAL_HIRED, CANCELED
+}
